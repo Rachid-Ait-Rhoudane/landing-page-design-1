193a092c0ecf66dade1f4d8facc92b16d6749dc1
Rachid-Ait-Rhoudane <rachid.aitrhoudane@etu.uae.ac.ma>
2023-09-06 19:35:22 +0100

    Improve the header section code

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
 let iconMenu = document.querySelector(".header .links .icon span:first-of-type");
+let iconMiddleHorizontalLigne = document.querySelector(".header .links .icon span:nth-of-type(3)");
 let menu  =document.querySelector(".header .links .icon + ul");
 
 iconMenu.addEventListener("click", function() {
     menu.classList.toggle("show");
+
+    if(menu.classList.contains("show")) {
+        iconMiddleHorizontalLigne.style.width = "100%";
+    } else {
+        iconMiddleHorizontalLigne.style.width = "50%";
+    }
+
 });
 
 window.addEventListener("click", function(e) {
     if(e.target.className !== "icon" && e.target.parentNode.className !== "icon") {
         menu.classList.remove("show");
+        iconMiddleHorizontalLigne.style.width = "50%";
     }
 });
